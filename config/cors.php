24528d44103739b46bcde3171ee6223d5f76183b
sasse853 <sassemahoussi@gmail.com>
2026-06-17 17:34:26 +0100

    Error fixed

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://btp-frontend-cr6o.onrender.com'],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:5173'),
+        'http://127.0.0.1:5173',
+        'https://btp-frontend-cr6o.onrender.com',
+    ],
 
     'allowed_origins_patterns' => [],
 
